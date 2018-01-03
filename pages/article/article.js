b849be8dfe4c6d2cf5d76023788051921378cf07
mabobj <mabobj@gmail.com>
2018-01-03 21:02:23 +0800

    up

--- pages/article/article.js
+++ pages/article/article.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/article/article.js
 Page({
 
   /**
--- pages/audio/audio.js
+++ pages/audio/audio.js
@@ -20,5 +20,20 @@ Page({
   },
   audioStart: function () {
     this.audioCtx.seek(0)
+  },
+  testCode:function(){
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.record']) {
+          wx.authorize({
+            scope: 'scope.record',
+            success() {
+              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+              wx.startRecord()
+            }
+          })
+        }
+      }
+    })
   }
 })
